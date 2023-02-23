f827368376b46abbf5f20582f3047c32d757fdeb
Kim <kim.gustyr@pandadoc.com>
2023-02-23 18:09:57 +0000

    feat: update gitignore, add newlines to templates, faster generation

--- scripts/insert_templates.py
+++ scripts/insert_templates.py
@@ -2,7 +2,7 @@
 import sys
 import re
 from pathlib import Path
-from typing import Dict, List, Optional
+from typing import Dict, Iterator, List, Optional
 
 PLACEHOLDER_LINE_REGEX = re.compile(r"<!-- tpl: ([a-zA-Z_]+) -->\n")
 
@@ -20,22 +20,32 @@ def get_template_name_from_line(line: str) -> Optional[str]:
         return match.group(1)
 
 
-def main():
-    assert len(sys.argv) == 3
-    _, input_file_path, output_file_path = sys.argv
-    rendered_templates = get_rendered_templates()
-    lines = []
+def _iter_lines(
+    input_file_path: str,
+    rendered_templates: Dict[str, List[str]],
+) -> Iterator[str]:
     with open(input_file_path, "r") as input_file:
         for line in input_file:
             template_name = get_template_name_from_line(line)
             if template_name:
                 rendered_lines = rendered_templates.get(template_name)
                 if rendered_lines:
-                    lines += rendered_lines
+                    yield from rendered_lines
                     continue
-            lines.append(line)
+            yield line
+
+
+def main():
+    assert len(sys.argv) == 3
+    _, input_file_path, output_file_path = sys.argv
+    rendered_templates = get_rendered_templates()
     with open(output_file_path, "w") as output_file:
-        output_file.writelines(lines)
+        output_file.writelines(
+            _iter_lines(
+                input_file_path=input_file_path,
+                rendered_templates=rendered_templates,
+            )
+        )
 
 
 if __name__ == "__main__":
